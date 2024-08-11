b2382ca44db1641d745d60fe22bcbcfde49c6e22
manu05X <k.manu00005@gmail.com>
2024-08-11 19:51:59 +0530

    Added DB to my server

--- server/index.js
+++ server/index.js
@@ -1,5 +1,17 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(
+    "mongodb+srv://kmanu00005:M9zCCiFCi4DS6RL1@cluster0.uj9da.mongodb.net/BMS?retryWrites=true&w=majority&appName=Cluster0"
+  )
+  .then(() => {
+    console.log("DB connection established");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const PORT = 8000;
 app.listen(PORT, () => {
