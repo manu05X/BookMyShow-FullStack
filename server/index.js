99b7a81fcfb95a52823e2bda8451d57a4aae3492
manu05X <k.manu00005@gmail.com>
2024-08-12 12:24:13 +0530

    Login routes completed

--- server/index.js
+++ server/index.js
@@ -1,19 +1,25 @@
 const express = require("express");
 const app = express();
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
+const connectDB = require("./config/db"); // Import the DB connection module
+const userRoutes = require("./routes/userRouter");
 
-mongoose
-  .connect(
-    "mongodb+srv://kmanu00005:M9zCCiFCi4DS6RL1@cluster0.uj9da.mongodb.net/BMS?retryWrites=true&w=majority&appName=Cluster0"
-  )
-  .then(() => {
-    console.log("DB connection established");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+//Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+// Connect to the database
+connectDB();
+
+//telling my app to use which routes for a request
+app.use("/api/users", userRoutes);
+
+//PORT Number
 const PORT = 8000;
+
+//Continusly Listening to Port
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
 });
+
+//https://github.com/mrinal1224/project/tree/master
