dd6ebe9a575f16bee5fe1965d3c69851068c84e1
manu05X <k.manu00005@gmail.com>
2024-08-15 23:16:43 +0530

    Adding the navBar of dashboard

--- client/src/component/ProtectedRoute.js
+++ client/src/component/ProtectedRoute.js
@@ -45,24 +45,78 @@ export default ProtectedRoute;
 */
 
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { hideLoading, showLoading } from "../redux/loaderSlice";
 import { getCurrentUser } from "../apicalls/users";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { setUser } from "../redux/userSlice";
+import { Layout, Menu, message } from "antd";
+import { Header } from "antd/es/layout/layout";
+import {
+  HomeOutlined,
+  UserOutlined,
+  ProfileOutlined,
+  LogoutOutlined,
+} from "@ant-design/icons";
 
 function ProtectedRoute({ children }) {
+  //using useSelector Hook
+  const { user } = useSelector((state) => state.user);
+  console.log("StateUser  ", user);
+
   // before seting the user we check if the user is valid user or not
   //our frontend need to make a api call
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
+  const navItems = [
+    {
+      label: "Home",
+      icon: <HomeOutlined />,
+    },
+
+    {
+      label: `${user ? user.name : " "}`,
+      icon: <UserOutlined />,
+
+      //nested Objects
+      children: [
+        {
+          label: (
+            <span
+              onClick={() => {
+                user.isAdmin ? navigate("/admin") : navigate("/profile");
+              }}
+            >
+              My Profile
+            </span>
+          ),
+          icon: <ProfileOutlined />,
+        },
+        {
+          label: (
+            <Link to="/login" onClick={() => localStorage.removeItem("token")}>
+              Log out
+            </Link>
+          ),
+          icon: <LogoutOutlined />,
+        },
+      ],
+    },
+  ];
+
   const getValidUser = async () => {
     try {
       dispatch(showLoading());
       const response = await getCurrentUser();
-      dispatch(hideLoading());
 
-      if (!response.success) {
+      if (response.success) {
+        dispatch(setUser(response.data)); // we send out user data to redux to be used in every component
+        dispatch(hideLoading());
+      } else {
+        dispatch(setUser(null));
+        message.error(response.message);
+        // dispatch(hideLoading());
         localStorage.removeItem("token");
         navigate("/login");
       }
@@ -82,7 +136,39 @@ function ProtectedRoute({ children }) {
     }
   }, [navigate]);
 
-  return <div>{children}</div>;
+  return (
+    user && (
+      <>
+        <Layout>
+          <Header
+            className="d-flex justify-content-between"
+            style={{
+              position: "sticky",
+              top: 0,
+              zIndex: 1,
+              width: "100%",
+              display: "flex",
+              alignItems: "center",
+            }}
+          >
+            <h3 className="demo-logo text-white m-0" style={{ color: "white" }}>
+              Book My Show
+            </h3>
+            <Menu theme="dark" mode="horizontal" items={navItems}></Menu>
+          </Header>
+          <div style={{ padding: 24, minHeight: 380, background: "#fff" }}>
+            {children}
+          </div>
+        </Layout>
+      </>
+    )
+
+    // <>
+    //   {/* <div>{children}</div>
+    //   {user ? <h1>{user.name}</h1> : <h1>Loading...</h1>} */}
+
+    // </>
+  );
 }
 
 export default ProtectedRoute;
